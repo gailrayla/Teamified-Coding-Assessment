0c5d85a23f0a5706278d36d84781f17b39fec235
Gail Rayla Emanuelle Parayno <paraynogail@gmail.com>
2025-03-30 00:48:20 +0800

    Sort and group transactions by date

--- src/components/layout/ContentWidthContainer/transaction-list/transaction-list.tsx
+++ src/components/layout/ContentWidthContainer/transaction-list/transaction-list.tsx
@@ -8,18 +8,38 @@ export const TransactionList = () => {
   useEffect(() => {
     fetch("http://localhost:3004/transactions")
       .then((res) => res.json())
-      .then((data) => setTransactions(data))
+      .then((data) => {
+        // sort transactions by date in descending order
+        const sortedTransactions = data.sort(
+          (a: Transaction, b: Transaction) =>
+            new Date(b.date).getTime() - new Date(a.date).getTime()
+        );
+        setTransactions(sortedTransactions);
+      })
       .catch((error) => console.error("Error fetching transactions:", error));
   }, []);
 
+  // group transactions by date
+  const groupedTransactions = transactions.reduce<
+    Record<string, Transaction[]>
+  >((acc, transaction) => {
+    acc[transaction.date] = acc[transaction.date] || [];
+    acc[transaction.date].push(transaction);
+    return acc;
+  }, {});
+
   return (
     <div>
-      <h2 style={{ fontSize: "16px", fontWeight: "normal" }}>Transactions</h2>
-      <ul>
-        {transactions.map((transaction) => (
-          <TransactionItem key={transaction.id} transaction={transaction} />
-        ))}
-      </ul>
+      {Object.entries(groupedTransactions).map(([date, transactions]) => (
+        <div key={date}>
+          <h2 style={{ fontSize: "16px", fontWeight: "normal" }}>{date}</h2>
+          <ul>
+            {transactions.map((transaction) => (
+              <TransactionItem key={transaction.id} transaction={transaction} />
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
