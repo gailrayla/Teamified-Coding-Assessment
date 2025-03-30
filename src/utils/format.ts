753b36697e656a48492c35b863be07898d9ecbe4
Gail Rayla Emanuelle Parayno <paraynogail@gmail.com>
2025-03-30 16:36:27 +0800

    Fix invalid date errro

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,8 +1,22 @@
 export const formatDate = (dateString: string): string => {
+  let dateObj;
+
+  if (dateString.includes("/")) {
+    const [day, month, year] = dateString.split("/");
+    dateObj = new Date(`${year}-${month}-${day}`);
+  } else {
+    dateObj = new Date(dateString);
+  }
+
+  if (isNaN(dateObj.getTime())) {
+    console.error("Invalid date:", dateString);
+    return "Invalid Date";
+  }
+
   const options: Intl.DateTimeFormatOptions = {
     year: "numeric",
     month: "long",
     day: "numeric",
   };
-  return new Date(dateString).toLocaleDateString(undefined, options);
+  return dateObj.toLocaleDateString(undefined, options);
 };
